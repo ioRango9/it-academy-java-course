5ef905eb229205949484f415ed16ab2c75ff6793
ioRango9 <igor97-11@mail.ru>
2022-05-31 01:56:29 +0300

    123

--- HelloJava.java
+++ HelloJava.java
@@ -1,6 +1,6 @@
 public class HelloJava {
 
     public static void main(String[] args) {
-        System.out.println("Hello World! and JAva");
+        System.out.println("Hello World! and JAva asd 123131 3 asd ");
     }
 }
