dbb2d5efa4a21ab811c845b12876f8b80099320a
ioRango9 <igor97-11@mail.ru>
2022-05-31 02:47:08 +0300

    About me

--- AboutMe.java
+++ AboutMe.java
@@ -1,4 +1,9 @@
 public class AboutMe {
 
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        String name = "Igor";
+        int age = 24;
+        double heightInMeters = 1.85;
+        System.out.printf("My name is %s\nA'm %d years\nMy height is %.2f meters", name, age, heightInMeters);
+    }
 }
