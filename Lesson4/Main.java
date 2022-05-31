9f2bc0b316cbe9335b9d55a85f85bc95f586157e
ioRango9 <igor97-11@mail.ru>
2022-05-31 12:51:27 +0300

    Second commit

--- Lesson4/Main.java
+++ Lesson4/Main.java
@@ -4,6 +4,11 @@ package Lesson4;
 public class Main {
     public static void main(String[] args) {
         int [] array = new int[] {5, 5, 1, 3, 6};
-        System.out.printf("%d, %d, %d, %d, %d", array[0], array[1], array[2], array[3], array[4]);
+        System.out.printf("%d, %d, %d, %d, %d\n", array[0], array[1], array[2], array[3], array[4]);
+        if (array[0] == 5) System.out.println("*****");
+        if (array[1] == 5) System.out.println("*****");
+        if (array[2] == 1) System.out.println("*");
+        if (array[3] == 3) System.out.println("***");
+        if (array[4] == 6) System.out.println("******");
     }
 }
