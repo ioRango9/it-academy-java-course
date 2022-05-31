1b902806c67808451709d8f95b089b22f87a1cdd
ioRango9 <igor97-11@mail.ru>
2022-05-31 11:59:54 +0300

    Second commit

--- Lesson4/Main.java
+++ Lesson4/Main.java
@@ -1,4 +1,9 @@
 package Lesson4;
 
+
 public class Main {
+    public static void main(String[] args) {
+        int [] array = new int[] {5, 5, 1, 3, 6};
+        System.out.printf("%d, %d, %d, %d, %d", array[0], array[1], array[2], array[3], array[4]);
+    }
 }
