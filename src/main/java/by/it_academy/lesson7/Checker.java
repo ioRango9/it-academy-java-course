6901f617bcad8b3e021e40b94c2269069144bda1
ioRango9 <igor97-11@mail.ru>
2022-06-10 11:44:56 +0300

    Lesson7 commit2

--- src/main/java/by/it_academy/lesson7/Checker.java
+++ src/main/java/by/it_academy/lesson7/Checker.java
@@ -4,6 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Checker {
+
     public boolean timeOfDay(String str) {
         int hh = 0;
         int mm = 0;
@@ -12,7 +13,6 @@ public class Checker {
         Pattern pattern = Pattern.compile("\\d+");
         Matcher matcher = pattern.matcher(str);
         int i = 0;
-        boolean timeCheck = false;
         while (matcher.find()) {
             int time_divide = 0;
             matcher.group();
@@ -20,39 +20,19 @@ public class Checker {
             i++;
             if (i == 1) {
                 hh = time_divide;
-                if (0 <= hh & hh < 24) {
-                    System.out.println("true");
-                } else {
-                    System.out.println("false");
-                    break;
-                }
             }
             if (i == 2) {
                 mm = time_divide;
-                if (0 <= mm & mm < 60) {
-                    System.out.println("true");
-                } else {
-                    System.out.println("false");
-                    break;
-                }
             }
             if (i == 3) {
                 ss = time_divide;
-                if (0 <= ss & ss < 60) {
-                    System.out.println("true");
-                    System.out.println(hh + ":" + mm + ":" + ss);
-                } else {
-                    System.out.println("false");
-                    break;
-                }
             }
         }
-        return timeCheck ;
+        return (hh >= 0 & hh < 24 & mm >= 0 & mm < 60 & ss >= 0 & ss < 60);
     }
 
-
     public static void main(String[] args) {
-        Checker checker = new Checker();
-        checker.timeOfDay("12:43:31");
+        Checker checkerTest = new Checker();
+        System.out.println(checkerTest.timeOfDay("23:25:58"));
     }
 }
