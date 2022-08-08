fa4dc6ddda332b8e53f41a39165cdc2f57c9e4c1
ioRango9 <106552889+ioRango9@users.noreply.github.com>
2022-08-08 12:22:01 +0300

    Delete IllegalStateException.java

--- src/main/java/by/it_academy/lesson11/IllegalStateException.java
+++
@@ -1,7 +0,0 @@
-package by.it_academy.lesson11;
-
-public class IllegalStateException extends RuntimeException {
-    public IllegalStateException() {
-        super("Exception! Sensor off");
-    }
-}
